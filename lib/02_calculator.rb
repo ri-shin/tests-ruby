a3ae5af1dd6aa88d75da6f627290929b37406e30
ri-shin <67656086+ri-shin@users.noreply.github.com>
2020-07-13 21:33:17 +0000

    Add files via upload

---
+++ lib/02_calculator.rb
@@ -0,0 +1,21 @@
+def add 
+    x = gets.chomp.to_i
+    y = gets.chomp.to_i
+
+    return x+y
+end
+
+def subtract
+    x = gets.chomp.to_i
+    y = gets.chomp.to_i
+    return x-y
+end
+
+
+def multiply
+    x = gets.chomp.to_i
+    y = gets.chomp.to_i
+    return x*y
+end
+
+
